9a1f1a9ad6469891e6af8c022a07cb309211a5cd
Garin Prakoso <gerin.rin@gmail.com>
2020-09-03 02:41:10 +0700

    update api_gateway.go

--- api_gateway/api_gateway.go
+++ api_gateway/api_gateway.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"net/http"
 	"tublessin/api_gateway/config/router"
 )
 
 func main() {
 	muxRouter := router.CreateRouter()
-	muxRouter.PathPrefix("/").Handler(http.FileServer(http.Dir("./frontend/")))
+	// muxRouter.PathPrefix("/").Handler(http.FileServer(http.Dir("./frontend/")))
 	router.NewAppRouter(muxRouter).InitRouter()
 	router.StartServer(muxRouter)
 }
